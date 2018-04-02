31fe7182dbe00f43e23571ba569699d6f61a2636
Aly Alidina <aly_alidina@hotmail.com>
2018-04-02 21:42:24 +0000

    creating curreny form params

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -2,27 +2,49 @@ require 'pry'
 
 class CurrencyController < ApplicationController
 
-  get '/currencies/index' do
+  get '/currencies' do
     @currency = Currency.all
-    if logged_in?
-      erb :'/currencies/index'
-    else
-      redirect '/login'
-    end
+    erb :'/currencies/index'
   end
 
-  post '/currencies/index' do
-    @currency.create(:name => params[:list][:name])
-  end
-
-
-  get '/lists/:list_id' do
+  get '/currencies/new' do
     @currency = Currency.all
+    erb :'currencies/new'
   end
-  post '/list' do
-     params[:item].inspect
-     redirect '/'
+
+  post '/currencies' do
+    binding.pry
   end
+
+
+  # get '/list' do
+  #   @currency = Currency.all
+  #   if logged_in?
+  #     erb :'/currencies/index'
+  #   else
+  #     redirect '/login'
+  #   end
+  # end
+  #
+  # post '/list' do
+  #   name = params.fetch("name")
+  #   redirect '/show'
+  # end
+  #
+  # patch '/list/:id' do
+  #
+  # end
+
+  # get '/lists/:list_id' do
+  #   @currency = Currency.all
+  #   @currency.index { |l| l.id == params[:crypto_id].to_i }
+  #   erb :show
+  # end
+  #
+  # post '/list' do
+  #    Currency.create(:name => params[:list][:name], :price => params[:price][:name] )
+  #    redirect '/list'
+  # end
 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 Currency.destroy_all
+#creating and saving two new instances of the Currency class
 
-currency1 = Currency.create(name: 'Bitcoin', price: '6,975.13')
-currency2 = Currency.create(name: 'Etherium', price: '394')
-currency3 = Currency.create(name: 'Neo', price: '51.10')
-currency4 = Currency.create(name: "Req", price: '0.1666')
-currency5 = Currency.create(name: "Ripple", price: '0.510967')
+Currency.create(name: "Bitcoin", price: "6,975.13")
+Currency.create(name: "Etherium", price: "394")
+Currency.create(name: "Neo", price: "51.10")
+Currency.create(name: "Req", price: "0.1666")
+Currency.create(name: "Ripple", price: "0.510967")
